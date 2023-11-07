ddec809c146b0e3311a60d373d2ccf3b2bafce61
xufeng <306885028@qq.com>
2023-11-07 16:41:13 +0800

    1

--- ccf/LSTM.py
+++ ccf/LSTM.py
@@ -3,20 +3,18 @@ from torch import nn
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size, batch_size, n_outputs,device):
+    def __init__(self, input_size, hidden_size, num_layers, output_size, batch_size,device):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.output_size = output_size
         self.num_directions = 1
-        self.n_outputs = n_outputs
         self.batch_size = batch_size
         self.device = device
-        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, batch_first=True,dropout=0.2)
+        self.lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, batch_first=True)
         # self.fcs = [nn.Linear(self.hidden_size, self.output_size).to(device) for i in range(self.n_outputs)]
-        self.fc1 = nn.Linear(self.hidden_size, self.output_size)
-        self.fc2 = nn.Linear(self.hidden_size, self.output_size)
+        self.fc = nn.Linear(self.hidden_size, self.output_size)
 
     def forward(self, input_seq):
         # print(input_seq.shape)
@@ -27,11 +25,7 @@ class LSTM(nn.Module):
         # input(batch_size, seq_len, input_size)
         # output(batch_size, seq_len, num_directions * hidden_size)
         output, _ = self.lstm(input_seq, (h_0, c_0))
-        pred1, pred2 = self.fc1(output), self.fc2(output)
-        pred1, pred2 = pred1[:, -1, :], pred2[:, -1, :]
-
-        # pred = torch.cat([pred1, pred2], dim=0)
-        pred = torch.stack([pred1, pred2], dim=0)
-        # print(pred.shape)
+        pred = self.fc(output)
+        pred = pred[:, -1, :]
 
         return pred
--- ccf/ccf.py
+++ ccf/ccf.py
@@ -3,9 +3,8 @@ import pandas as pd
 import torch.nn as nn1
 
 from sklearn import preprocessing
+from torch import nn
 from torch.utils.data import Dataset, DataLoader
-from torch_geometric import nn
-from torch_geometric.data import Data
 import torch.nn.functional as F
 from GAT import GAT
 from LSTM import LSTM
@@ -33,7 +32,7 @@ for col in node_data.columns[2:]:
     node_data[col] = np.clip(node_data[col], lower_bound, upper_bound)
 
 # 替换边数据中的异常值
-for col in edge_data.columns[2:]:
+for col in edge_data.columns[2:4]:
     col_mean = edge_data[col].mean()
     col_std = edge_data[col].std()
     lower_bound = col_mean - 6 * col_std
@@ -48,9 +47,12 @@ node = []
 label=[]
 for _, node_data in grouped_node_data:
     node_data.iloc[:, 0] = le.fit_transform(node_data.iloc[:, 0])
+    node_data=node_data.sort_values(by='geohash_id')
     node.append(node_data.iloc[:, [0] + list(range(2, 37))])
-    y=torch.FloatTensor(node_data.iloc[:,[-2,-1]].values)
+    y=torch.FloatTensor(node_data.iloc[:,[-2,-1]].values).reshape(-1)
     label.append(y)
+label=torch.stack(label)
+# print(label.shape)
 grouped_edge_data = edge_data.groupby('date_id')
 edge = []
 for _, edge_data in grouped_edge_data:
@@ -65,127 +67,83 @@ h_size=64
 num_heads=8
 edge_dim=2
 model=GAT(in_feats=in_size,h_feats=h_size,out_feats=out_size,edge_dim=edge_dim)
-model.to(device)
-X=[]
+# model.to(device)
+data=[]
 for i in range(len(edge)):
-    sorted_node = node[i].sort_values(by='geohash_id')
+    sorted_node = node[i]
     x = sorted_node.iloc[:, 1:].values
-    node_num = len(sorted_node)
     edges = edge[i]
     # 边的头结点
     edge_index1 = edges[['geohash6_point1']].values.astype(int)
     # 边的尾结点
     edge_index2 = edges[['geohash6_point2']].values.astype(int)
     edge_attr = edges[['F_1', 'F_2']].values
-    x = torch.FloatTensor(x).to(device)
+    x = torch.FloatTensor(x)#.to(device)
     edge_index = np.array([edge_index1, edge_index2])
-    edge_index = torch.LongTensor(edge_index).reshape(2,-1).to(device)
-    edge_attr = torch.from_numpy(edge_attr).float().to(device)
+    edge_index = torch.LongTensor(edge_index).reshape(2,-1)#.to(device)
+    edge_attr = torch.from_numpy(edge_attr).float()#.to(device)
     # print(x.shape)
     # print(edge_index.shape)
     # print(edge_attr.shape)
-    node_emb=model(x,edge_index,edge_attr)
-
-
-
-
+    X=model(x,edge_index,edge_attr).view(-1)
+    # print(X.shape)#(1140*out_size)
+    data.append(X)
+data=torch.stack(data)
+# print(data.shape)
 class MyDataset(Dataset):
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
+    def __init__(self, data):
+        self.data = data
+
+    def __getitem__(self, item):
+        return self.data[item]
 
     def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, index):
-        return self.X[index], self.y[index]
+        return len(self.data)
 
 
 def create_sliding_windows(data,label, window_size):
-    X,y=[],[]
+    seq=[]
     for i in range(len(data) - window_size):
-        X.append(data[i:i + window_size,:])
-        y.append(label[i + window_size])
-        X = torch.FloatTensor(X)
-        y = torch.FloatTensor(y)
-    return X,y
+        X=data[i:i + window_size]
+        y=label[i + window_size]
+        seq.append((X,y))
+    return seq
 
+#设置随机数种子
+torch.manual_seed(1234)
+#窗口大小
+window_size = 5
+batch_size = 1
+seq=create_sliding_windows(data,label,window_size)
+# print(X.shape)
+# print(y.shape)
+dataset=MyDataset(seq)
+dataloader=DataLoader(dataset,batch_size=batch_size,shuffle=True,drop_last=True)
+input_size=len(data[0])
+out_size=label.shape[1]
 
-def train_model(model, X_train, y_train, num_epochs):
-        loss_fn = torch.nn.MSELoss(reduction='sum')
-        optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
-        for t in range(num_epochs):
-            model.hidden = model.reset_hidden_state()
-            y_pred = model(X_train)
-            loss = loss_fn(y_pred.float().to(device), y_train)
+model = LSTM(batch_size=batch_size, input_size=input_size, hidden_size=64, num_layers=1, output_size=out_size,device=device)
+model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.MSELoss()
 
-            optimiser.zero_grad()
-            loss.backward()
-            optimiser.step()
+num_epochs = 100
+model.train()
+for epoch in range(num_epochs):
+    for data in dataloader:
+        inputs, targets = data
+        inputs, targets = inputs.to(device), targets.to(device)
 
-            if t % 10 == 0:
-                print(f'Epoch {t} train loss: {loss.item()}')
+        # 前向传播
+        outputs = model(inputs)
 
-        return model.eval()
+        # 计算损失
+        loss = criterion(outputs, targets)
 
-class LSTMModel(nn1.Module):
-    def __init__(self, input_size, hidden_size, num_layers, seq_len):
-        super(LSTMModel, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.seq_len = seq_len
-        self.lstm = nn1.LSTM(input_size, hidden_size, num_layers, dropout=0.5)
-        self.linear = nn1.Linear(in_features=hidden_size,out_features=1)
-        self.linear1 = nn1.Linear(in_features=hidden_size, out_features=1)
-        self.linear2 = nn1.Linear(in_features=hidden_size, out_features=1)
+        # 反向传播和优化
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-
-
-
-    def forward(self, int_put):
-        out_put = self.lstm(int_put)
-        out_put = self.fc(out_put)
-        pre1 = self.linear1(out_put)
-        pre2 = self.linear2(out_put)
-        pre1 = pre1[:,-1]
-        pre2 = pre2[:,-2]
-        return [pre2,pre1]
-
-
-
-
-
-
-# #设置随机数种子
-# torch.manual_seed(1234)
-# #窗口大小
-# window_size = 5
-# batch_size = 3
-# X,y=create_sliding_windows(data,widow_size)
-# dataset=MyDataset(X,y)
-# dataloader=DataLoader(dataset,batch_size=batch_size,shuffle=False)
-#
-# model = LSTM(batch_size=batch_size, input_size=35, hidden_size=32, num_layers=1, output_size=35)
-# model.to(device)
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-# criterion = nn.MSELoss()
-#
-# num_epochs = 100
-# model.train()
-# for epoch in range(num_epochs):
-#     for data in seq:
-#         inputs = data[0].to(device)
-#         targets = data[1].to(device)
-#         # 前向传播
-#         outputs = model(inputs)
-#
-#         # 计算损失
-#         loss = criterion(outputs, targets)
-#
-#         # 反向传播和优化
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#     if (epoch + 1) % 10 == 0:
-#         print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+    # 每个epoch结束后打印损失值
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
